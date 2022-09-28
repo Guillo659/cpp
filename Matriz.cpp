3f557d594453e69fe83757a5bbe410147cffc17c
GuilloSalgado <68713952+Guillo659@users.noreply.github.com>
2022-09-27 20:31:15 -0500

    charge matriz.cpp
    Ejercicio con Matrices en C++.

---
+++ Matriz.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+  int f, c;
+  
+  do{
+    std::cout<<"Humano ingresa el número de Filas de tu Matriz: ";std::cin>>f;
+    std::cout<<"Humano ingresa el número de Columnas de tu Matriz: ";std::cin>>c;    
+  } while((f<=0 o f>10)||(c<=0 o c>10));
+  
+  int array [f][c];
+  
+  for(int i = 0; i = f; i++){
+    for(int j = 0; j< c; j++){
+      std::cout<<"Humano ingresa el valor de la posición"<<(i+1)<<","<<(j+1)<<" : ";
+      std::cin>>array[i][j];
+    }
+  } for(int i = 0; i = f; i++){
+      for(int j = 0; j = c; j++){
+        std::cout<<array[i][j]<<" ";
+      }
+      std::cout<<"\n";
+  }
+  std::cout<<"\n";
+  
+  system("pause");
+  return 0;
+}
